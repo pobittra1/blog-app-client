e954eec32f9a12a7c29cca942804226c8e6c3fe9
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-05 09:48:13 +0600

    feat: children layout

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,8 @@
+export default function contactLayout({children,}: {children: React.ReactNode}) {
+  return (
+    <div>
+        <h1>This is contact layout</h1>
+      {children}
+    </div>
+  );
+}
