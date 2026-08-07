ef71752eea6c25369c6d54a7bf5ddfda2cc1f806
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-07 09:21:09 +0600

    feat: understand about error and loading page route

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,10 @@
-export default function About() {
+export default async function AboutPage() {
+  await new Promise ((resolve) => setTimeout(resolve, 4000));
+
+  throw new Error("Smoething went wrong!");
   return (
     <div>
-      <h1>About</h1>
+      <h1>this is about page component</h1>
     </div>
   );
 }
