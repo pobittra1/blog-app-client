cee2060dc4b0f216cb088b140ea024f65a8991e1
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-07 10:01:50 +0600

    docs: understand static and dynamic

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,10 @@
-export default async function AboutPage() {
-  await new Promise ((resolve) => setTimeout(resolve, 4000));
+export const dynamic = "force-dynamic";
 
-  throw new Error("Smoething went wrong!");
+export default async function AboutPage() {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+
+  // For simulating error
+  // throw new Error("Smoething went wrong!");
   return (
     <div>
       <h1>this is about page component</h1>
