810c7eb520c0d51614740439b53bce7f2b3b8419
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-08 23:46:27 +0600

    refactor: remove console

--- src/components/modules/authentication/login-form.tsx
+++ src/components/modules/authentication/login-form.tsx
@@ -31,7 +31,7 @@ export function LoginForm({
     console.log(data);
   };
   const session = authClient.useSession();
-  console.log(session);
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
