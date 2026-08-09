58e444ccf3385e0e48fcd673a1b761cbcf9d051d
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-09 13:36:33 +0600

    protect routes using proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from "./constans/roles";
 
 export async function proxy(request: NextRequest){
-console.log("Hello from proxy: ",request.url);
-    return NextResponse.next();
+    const pathName = request.nextUrl.pathname;
+    let isAuthenticated = false;
+    let isAdmin = false;
+const {data}= await userService.getSession();
+if(data){
+    isAuthenticated = true;
+    isAdmin = data.user.role === Roles.admin;
+}
 
+// user in not authenticated at all
+if(!isAuthenticated){
+    return NextResponse.redirect(new URL("/login", request.url))
+}
+// user is authenticated and role = ADMIN
+// ADMIN can not visit USER dashboard
+if(isAdmin && pathName.startsWith("/dashboard")){
+    return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+}
+// user is authenticated and role = USER
+// USER can not visit ADMIN dashboard
+if(!isAdmin && pathName.startsWith("/admin-dashboard")){
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+}
+return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/dashboard"],
+    matcher: ["/dashboard","/dashboard/:path*", 
+        "/admin-dashboard", "/admin-dashboard/:path*"],
 }
