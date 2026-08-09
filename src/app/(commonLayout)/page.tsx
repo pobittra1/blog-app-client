2ae3dfe48eafccb3f6cd13e8b3a9eae28b5a732d
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-09 09:25:54 +0600

    get cookies from server to client

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const session = await authClient.getSession();
-  console.log(session);
+  const cookieStore = await cookies();
+  console.log(cookieStore.get("better-auth.session_token"));
+  const res = await fetch("http://localhost:4000/api/auth/get-session");
+  console.log(res.json());
   return (
     <div>
       <Button variant={"outline"}>Click Here</Button>
