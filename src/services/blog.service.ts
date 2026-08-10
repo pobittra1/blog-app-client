2cdfbf1a1d078fe2a51b4b127f1d94826ce20384
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-10 21:44:02 +0600

    see error

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -44,6 +44,7 @@ export const blogService = {
       return { data: data, error: null };
     } catch (err) {
       return { data: null, error: { message: "Something went wrong!" } };
+      console.log(err);
     }
   },
 };
