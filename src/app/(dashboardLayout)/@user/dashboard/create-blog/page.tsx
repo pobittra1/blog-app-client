edc4f1a23cbc4599c6a3cc8e1a07006d1fd4a7b6
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-13 10:35:16 +0600

    understand post flow and fixing role issue

---
+++ src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateBlogPage() {
+  return (
+    <div>
+      <h1>this is create blog Page</h1>
+    </div>
+  );
+}
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,13 @@
 import { Route } from "@/types";
 
 export const userRoutes: Route[] = [
-    {
-      title: "User Management",
-      items: [
-        {
-          title: "Create Blog",
-          url: "/create-blog",
-        },
-      ],
-    },
-  ]
+  {
+    title: "User Management",
+    items: [
+      {
+        title: "Create Blog",
+        url: "/dashboard/create-blog",
+      },
+    ],
+  },
+];
