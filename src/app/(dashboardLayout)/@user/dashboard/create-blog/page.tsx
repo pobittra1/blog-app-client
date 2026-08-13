697ce0de6ba5bc098f1feaf318ec4d57dd502ee6
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-13 11:30:17 +0600

    add cache in blogPosts

--- src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
@@ -1,9 +1,16 @@
 import CreateBlogFormServer from "@/components/modules/user/createBlog/CreateBlogFormServer";
+import { blogService } from "@/services/blog.service";
+import { BlogPost } from "@/types";
 
-export default function CreateBlogPage() {
+export default async function CreateBlogPage() {
+  const { data } = await blogService.getBlogPost({}, { cache: "no-store" });
+
   return (
     <div>
       <CreateBlogFormServer></CreateBlogFormServer>
+      {data.data.map((item: BlogPost) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
     </div>
   );
 }
