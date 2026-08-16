9cde337f3e8d68705309cccafdb21e8981aadd8c
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-16 22:58:54 +0600

    remove console

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,27 +1,26 @@
-import { env } from './../../env';
+import { env } from "./../../env";
 import { cookies } from "next/headers";
 const AUTH_URL = env.AUTH_URL;
 
 export const userService = {
-    getSession: async function () {
-try{
-const cookieStore = await cookies();
-  console.log(cookieStore.toString());
-  const res = await fetch(`${AUTH_URL}/get-session`, {
-    headers: {
-      Cookie: cookieStore.toString(),
-    },
-    cache: "no-store",
-  });
-  const session = await res.json();
-  if(session === null){
-    return {data: null, error: {message: "Session is missing"}}
-  }
- return {data: session, error: null};
-
-}catch(err){
-console.error(err);
-return {data: null, error: {message: "Something went wrong"}}
-}
+  getSession: async function () {
+    try {
+      const cookieStore = await cookies();
+      console.log(cookieStore.toString());
+      const res = await fetch(`${AUTH_URL}/get-session`, {
+        headers: {
+          Cookie: cookieStore.toString(),
+        },
+        cache: "no-store",
+      });
+      const session = await res.json();
+      if (session === null) {
+        return { data: null, error: { message: "Session is missing" } };
+      }
+      return { data: session, error: null };
+    } catch (err) {
+      // console.error(err);
+      return { data: null, error: { message: "Something went wrong" } };
     }
-}
+  },
+};
