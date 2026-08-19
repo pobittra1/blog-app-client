1b2823d163c3ee7d0735097b508ad04f8eff6a80
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-19 20:54:38 +0600

    create history route and page. also table for history

--- src/components/modules/user/history/HistoryTable.tsx
+++ src/components/modules/user/history/HistoryTable.tsx
@@ -1,7 +1,33 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default function HistoryTable() {
   return (
     <div>
-      <h1>HistoryTable</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead>Tags</TableHead>
+            <TableHead>Views</TableHead>
+            <TableHead>Comments</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
