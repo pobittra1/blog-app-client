464fb07b7474e28e67de457c2068aee6a87e4b78
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-20 10:34:37 +0600

    create pagination controls and pass pagination data from history page

--- src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
@@ -1,4 +1,5 @@
 import HistoryTable from "@/components/modules/user/history/HistoryTable";
+import PaginationControls from "@/components/ui/pagination-controls";
 import { blogService } from "@/services/blog.service";
 
 export default async function HisotoryPage({
@@ -10,11 +11,19 @@ export default async function HisotoryPage({
   const response = await blogService.getBlogPost({ page });
 
   const posts = response.data?.data || [];
+  const pagination = response.data?.pagination || {
+    limit: 10,
+    page: 1,
+    total: 0,
+    totalPages: 1,
+  };
 
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-6">Blog Post History</h1>
       <HistoryTable posts={posts}></HistoryTable>
+
+      <PaginationControls meta={pagination}></PaginationControls>
     </div>
   );
 }
