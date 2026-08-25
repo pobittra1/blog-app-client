16a01ed6dc5349412a485320c6c7d9fa8cc21f0f
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-25 21:46:53 +0600

    refactor search form

--- src/components/layout/search-form.tsx
+++ src/components/layout/search-form.tsx
@@ -14,7 +14,7 @@ export function SearchForm({ ...props }: React.ComponentProps<"form">) {
       <SidebarGroup className="py-0">
         <SidebarGroupContent className="relative">
           <Label htmlFor="search" className="sr-only">
-            Search Here
+            Search
           </Label>
           <SidebarInput
             id="search"
